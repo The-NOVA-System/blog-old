ab482a2490705cab29b8e94c22deddea33e8ac2f
Garv <gshah.6110@gmail.com>
2021-07-22 19:33:58 +1000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,6 @@ window.onload = function () {
     var script = document.createElement('script');
     var firstScript = document.getElementsByTagName('script')[0];
     script.async = true;
-    script.src = '{{'sw-register.js'|relative_url}}?v=' + Date.now();
+    script.src = './sw-register.js?v=' + Date.now();
     firstScript.parentNode.insertBefore(script, firstScript);
 };
