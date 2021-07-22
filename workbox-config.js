fae87f2a1136b368aefa4e22a770eb35dda4e42c
Restyled.io <commits@restyled.io>
2021-07-22 23:38:17 +0000

    Restyled by prettier

--- workbox-config.js
+++ workbox-config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  globDirectory : '_site/',
-  globPatterns : [ '**/*.{css,png,js,mjs,xml,html,json,md,gif}' ],
-  ignoreURLParametersMatching : [ /^utm_/, /^fbclid$/],
-  swDest : './sw.js',
-  runtimeCaching : [ {
-    // Match any request that ends with .png, .jpg, .jpeg or .svg.
-    urlPattern : /\.(?:png|jpg|jpeg|svg)$/,
+  globDirectory: "_site/",
+  globPatterns: ["**/*.{css,png,js,mjs,xml,html,json,md,gif}"],
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  swDest: "./sw.js",
+  runtimeCaching: [
+    {
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
 
-    // Apply a cache-first strategy.
-    handler : 'CacheFirst',
+      // Apply a cache-first strategy.
+      handler: "CacheFirst",
 
-    options : {
-      // Use a custom cache name.
-      cacheName : 'images',
+      options: {
+        // Use a custom cache name.
+        cacheName: "images",
 
-      // Only cache 10 images.
-      expiration : {
-        maxEntries : 15,
+        // Only cache 10 images.
+        expiration: {
+          maxEntries: 15,
+        },
       },
     },
-  } ],
+  ],
 };
