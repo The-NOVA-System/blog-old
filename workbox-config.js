b082f513f4eb49fe77551f6f14b435676d6fbaa0
Garv <gshah.6110@gmail.com>
2021-07-23 09:35:01 +1000

    using workbox

---
+++ workbox-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	globDirectory: '_site/',
+	globPatterns: [
+		'**/*.{css,png,js,mjs,xml,html,json,md,gif}'
+	],
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	],
+	swDest: './sw.js',
+	runtimeCaching: [{
+		// Match any request that ends with .png, .jpg, .jpeg or .svg.
+		urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+		// Apply a cache-first strategy.
+		handler: 'CacheFirst',
+
+		options: {
+			// Use a custom cache name.
+			cacheName: 'images',
+
+			// Only cache 10 images.
+			expiration: {
+				maxEntries: 15,
+			},
+		},
+	}],
+};
